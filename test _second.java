8e27c0df05f53ebf912ebb8f1b8506534db74d64
gaju-8050 <108262877+gaju-8050@users.noreply.github.com>
2022-06-29 13:13:53 +0530

    Update test _second.java

--- test _second.java
+++ test _second.java
@@ -3,4 +3,7 @@ class first
    public static void main(String[] args)
 {
     System.out.println("HELLO THIS IS NEW PROJECT);
+    System.out.ptintln("SOME THINGS ARE CHANGED");
+    System.out.ptintln("BYE!!");
+                       
 }
